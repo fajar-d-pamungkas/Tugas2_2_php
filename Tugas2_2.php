dd2350e63681b9f35a78b074f82f56a3ca7f3305
fajar-d-pamungkas <71364634+fajar-d-pamungkas@users.noreply.github.com>
2020-10-08 09:48:13 +0700

    Add files via upload

---
+++ Tugas2_2.php
@@ -0,0 +1,10 @@
+<?php
+$beli = 50000;
+$jual = 80000;
+$untung = (($jual-$beli)/$beli)*100;
+
+echo "Jual = $jual <br>";
+echo "Beli = $beli <br>";
+echo "Keuntungan = ? <br>";
+echo "Jawaban : $untung%";
+?>
